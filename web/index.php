49450ed6f3bddb279fb15074af0feddecf3118ef
FerOrdSom <fordsom@gmail.com>
2022-04-04 19:04:12 +0200

    first changes

--- web/index.php
+++ web/index.php
@@ -1,17 +1,42 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Gestor de arte</title>
-    </head>
-    <body>
-        <?php
-        echo "Aqui el inicio";
-        ?>
-    </body>
-</html>
+<?php
+
+// web/index.php
+// carga del modelo y los controladores
+require_once __DIR__ . '/../app/Config.php';
+require_once __DIR__ . '/../app/Model.php';
+require_once __DIR__ . '/../app/Controller.php';
+// enrutamiento
+$map = array(
+    'inicio' => array('controller' => 'Controller', 'action' => 'inicio'),
+    'listar' => array('controller' => 'Controller', 'action' => 'listar'),
+    'insertar' => array('controller' => 'Controller', 'action' => 'insertar'),
+    'buscar' => array('controller' => 'Controller', 'action' => 'buscarPorNombre'),
+    'ver' => array('controller' => 'Controller', 'action' => 'ver')
+);
+// Parseo de la ruta
+if (isset($_GET['ctl'])) {
+    if (isset($map[$_GET['ctl']])) {
+        $ruta = $_GET['ctl'];
+    } else {
+        header('Status: 404 Not Found');
+        echo '<html><body><h1>Error 404: No existe la ruta <i>' .
+        $_GET['ctl'] .
+        '</p></body></html>';
+        exit;
+    }
+} else {
+    $ruta = 'inicio';
+}
+$controlador = $map[$ruta];
+// Ejecución del controlador asociado a la ruta
+if (method_exists($controlador['controller'], $controlador['action'])) {
+    call_user_func(array(new $controlador['controller'],
+        $controlador['action']));
+} else {
+    header('Status: 404 Not Found');
+    echo '<html><body><h1>Error 404: El controlador <i>' .
+    $controlador['controller'] .
+    '->' .
+    $controlador['action'] .
+    '</i> no existe</h1></body></html>';
+}
