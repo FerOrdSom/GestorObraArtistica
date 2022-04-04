a463e8162c6698d4e89e26b36c168a9b11d530fa
FerOrdSom <fordsom@gmail.com>
2022-04-04 18:56:32 +0200

    inicio

---
+++ web/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Gestor de arte</title>
+    </head>
+    <body>
+        <?php
+        echo "Aqui el inicio";
+        ?>
+    </body>
+</html>
