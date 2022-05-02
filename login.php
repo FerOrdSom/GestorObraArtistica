8ecc5cad50a679dca8a3542be6585347f2c8cdf4
FerOrdSom <fordsom@gmail.com>
2022-05-02 19:19:38 +0200

    php connection to database

--- login.php
+++ login.php
@@ -1,3 +1,12 @@
 <?php
 readfile("login.html");
 ?>
+
+<?php
+    $mysqli = new mysqli("localhost", "root", "", "gestion");
+    if ($mysqli){
+      echo "dev info: connection succeded";
+    }else{
+      echo "dev info: connection failed";
+    }
+?>
