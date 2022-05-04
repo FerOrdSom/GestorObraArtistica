48698a883f682837934333fea578d4db4b6d0afa
FerOrdSom <fordsom@gmail.com>
2022-05-04 03:52:46 +0200

    register logic

--- login.php
+++ login.php
@@ -9,6 +9,7 @@
 </head>
 
 <body>
+<!--login form -->
 <form action="login.php" method="get" class="form-example">
   <div class="login-form">
     <label for="username">Introduzca usuario: </label>
@@ -22,6 +23,29 @@
     <input type="submit" value="Login">
   </div>
 </form>
+<!--register form -->
+<form action="login.php" method="get" class="form-example">
+  <div class="register-form">
+    <label for="newusername">Introduzca nombre de usuario: </label>
+    <input type="text" name="newusername" required>
+  </div>
+  <div class="register-form">
+    <label for="newemail">Introduzca su email: </label>
+    <input type="email" name="newemail" required>
+  </div>
+  <div class="register-form">
+    <label for="newpassword">Introduzca su password: </label>
+    <input type="text" name="newpassword" required>
+  </div>
+  <div class="register-form">
+    <label for="checkpassword">Confirme su password: </label>
+    <input type="text" name="checkpassword" required>
+  </div>
+  <div class="login-form">
+    <input type="submit" value="Registrar">
+  </div>
+</form>
+
 </body>
 </html>
 
@@ -50,6 +74,7 @@ while ($row = $result->fetch_object()){
 
  <?php
 //Creating basic login logic with prepared statements
+if (isset($_GET["username"]) && isset($_GET["password"])){
 $user = $_GET["username"];
 $password = $_GET["password"];
 echo "dev info: usuario introducido: ".$user;
@@ -75,6 +100,33 @@ if (!isset($username)){
 }else{
   header('Location: index.php');
 }
+}
+?>
+<?php
+//Creating basic register logic with prepared statements
+if (isset($_GET["newusername"]) &&
+        isset($_GET["newemail"]) &&
+        isset($_GET["newpassword"]) &&
+        isset($_GET["checkpassword"])){
+$newusername = $_GET["newusername"];
+$newemail = $_GET["newemail"];
+$newpassword = $_GET["newpassword"];
+$checkpassword = $_GET["checkpassword"];
 
+//basic password check
+if ($newpassword != $checkpassword){
+  echo "el password no coincide";
+}else{
+echo "<br>dev info: "."newuser: ".$newusername." newemail: ".$newemail." newpass: ".$newpassword. " checkpass: ".$checkpassword;
 
-  ?>
+/* Prepared statement, stage 1: prepare */
+$stmt = $mysqli->prepare("INSERT INTO users(name, email, password) VALUES (?, ?, ?)");
+
+/* Prepared statement, stage 2: bind and execute */
+
+$stmt->bind_param("sss", $newusername, $newemail, $newpassword); // "is" means that $id is bound as an integer and $label as a string
+
+$stmt->execute();
+}
+}
+ ?>
